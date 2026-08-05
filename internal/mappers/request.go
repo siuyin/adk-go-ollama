26dbdf50f4ade5a93bdb62e886261f23f8c6bcfb
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-08-05 03:50:02 +0000

    fix: revert to false due to ollama bug

--- internal/mappers/request.go
+++ internal/mappers/request.go
@@ -62,7 +62,7 @@ func ChatRequestFromLLMRequest(modelID string, req *model.LLMRequest, stream boo
 		Messages: messages,
 		Stream:   &stream,
 		Options:  optionsFromGenai(cfg),
-		Think:    &ollamaapi.ThinkValue{Value: "low"},
+		Think:    &ollamaapi.ThinkValue{Value: false},
 	}
 
 	if tools := toolsFromGenai(cfg); len(tools) > 0 {
